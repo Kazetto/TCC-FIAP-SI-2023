1153ebe5ac74f45b698ea42226a24a4e6320acb4
Andre Keigo Nishioka <andre_keigo@hotmail.com>
2023-07-15 23:04:58 -0300

    Continuando o html, css e javascript
    Continiando o processo de front end do projeto

---
+++ TCC/javascript/login.js
@@ -0,0 +1,23 @@
+function voltar() {
+    window.history.back();
+  }
+
+function validarFormulario(event) {
+    event.preventDefault();
+  
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+  
+    if (usuario === "" || senha === "") {
+      alert("Por favor, preencha todos os campos.");
+    } else {
+      // Aqui você pode adicionar a lógica adicional para validar o usuário e a senha.
+      // Por exemplo, fazer uma solicitação para um servidor, comparar com valores predefinidos, etc.
+      //alert("Formulário válido. Usuário: " + usuario + ", Senha: " + senha);
+  
+      // Redirecionar para a próxima página após a validação bem-sucedida
+      window.location.href = "/pages/configuracao.html";
+    }
+}
+  
+  
